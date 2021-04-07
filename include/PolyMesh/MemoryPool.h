07fb249b4723b2b948154f20f32d5d37bda09a9d
jingma-git <11921154@zju.edu.cn>
2021-04-07 23:15:50 +0800

    comment on addPolygonFace()

--- include/PolyMesh/MemoryPool.h
+++ include/PolyMesh/MemoryPool.h
@@ -4,52 +4,53 @@
 
 namespace acamcad
 {
-	template< typename T>
+	template <typename T>
 	class MemoryPool
 	{
 	private:
 		size_t BLOCK_SIZE = 1024;
 
-		std::vector<T*> blocks;
-		std::vector<T*> freeAdresses;
+		std::vector<T *> blocks;
+		std::vector<T *> freeAdresses;
 
 	public:
 		MemoryPool() : BLOCK_SIZE(1024) {}
 		~MemoryPool() { Clear(); }
 
 	public:
-		T* Request();
-		void Recycle(T* object);
+		T *Request();
+		void Recycle(T *object);
 		void Reserve(size_t n);
 		// no ~T()
 		void fastClear();
-		void Clear();	//依次调用所有的析构函数，然后归还空间
+		void Clear(); // call deconstructor per element and then free space
 
 	private:
 		void newBlock();
-
 	};
 
-	template<typename T>
-	T* MemoryPool<T>::Request()
+	template <typename T>
+	T *MemoryPool<T>::Request()
 	{
+		// consume a space
 		if (freeAdresses.empty())
 			newBlock();
-		T* freeAdress = freeAdresses.back();
+		T *freeAdress = freeAdresses.back();
 		freeAdresses.pop_back();
 		return freeAdress;
 	}
 
-	template<typename T>
-	void MemoryPool<T>::Recycle(T* object) 
+	template <typename T>
+	void MemoryPool<T>::Recycle(T *object)
 	{
-//		if constexpr (!std::is_trivially_destructible_v<T>)
-//			object->~T();
+		//		if constexpr (!std::is_trivially_destructible_v<T>)
+		//			object->~T();
+		// return a space
 		object->~T();
 		freeAdresses.push_back(object);
 	}
 
-	template<typename T>
+	template <typename T>
 	void MemoryPool<T>::Reserve(size_t n)
 	{
 		size_t blockNum = n / BLOCK_SIZE + static_cast<size_t>(n % BLOCK_SIZE > 0);
@@ -57,23 +58,27 @@ namespace acamcad
 			newBlock();
 	}
 
-	template<typename T>
-	void MemoryPool<T>::fastClear() 
+	template <typename T>
+	void MemoryPool<T>::fastClear()
 	{
-		for (auto block : blocks) {
+		for (auto block : blocks)
+		{
 			free(block);
 		}
 		blocks.clear();
 		freeAdresses.clear();
 	}
 
-	template<typename T>
-	void MemoryPool<T>::Clear() 
+	template <typename T>
+	void MemoryPool<T>::Clear()
 	{
-		std::unordered_set<T*> freeAdressesSet(freeAdresses.begin(), freeAdresses.end());
-		for (auto block : blocks) {
-			for (size_t i = 0; i < BLOCK_SIZE; i++) {
-				T* adress = block + i;
+		std::unordered_set<T *> freeAdressesSet(freeAdresses.begin(), freeAdresses.end());
+		for (auto block : blocks)
+		{
+			for (size_t i = 0; i < BLOCK_SIZE; i++)
+			{
+				T *adress = block + i;
+				// element whose address not in freeAddresses should call deconstructor to clear its content
 				if (freeAdressesSet.find(adress) == freeAdressesSet.end())
 					adress->~T();
 			}
@@ -83,11 +88,12 @@ namespace acamcad
 		freeAdresses.clear();
 	}
 
-	template<typename T>
-	void MemoryPool<T>::newBlock() {
-//		T* block = (T*)operator new(sizeof(T)* BLOCK_SIZE);
+	template <typename T>
+	void MemoryPool<T>::newBlock()
+	{
+		//		T* block = (T*)operator new(sizeof(T)* BLOCK_SIZE);
 
-		T* block = reinterpret_cast<T*>( malloc(BLOCK_SIZE * sizeof(T)) );
+		T *block = reinterpret_cast<T *>(malloc(BLOCK_SIZE * sizeof(T)));
 
 		blocks.push_back(block);
 		for (size_t i = 0; i < BLOCK_SIZE; i++)
