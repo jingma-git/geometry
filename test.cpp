7827b49e72bafbca4261ac085d209b99214da8cc
jingma-git <11921154@zju.edu.cn>
2021-09-10 15:23:49 +0800

    edge links of mesh

--- test.cpp
+++ test.cpp
@@ -1,8 +1,10 @@
 
 #include <egl/readOBJ.h>
+#include <egl/edge_topology.h>
 #include <iostream>
 using namespace Eigen;
 using namespace std;
+using namespace egl;
 
 int main()
 {
@@ -12,5 +14,10 @@ int main()
     egl::readOBJ("data/plane.obj", V, F);
     cout << V.rows() << ", " << V.cols() << endl;
     cout << F.rows() << ", " << F.cols() << endl;
+    MatrixXi in_edges, bnd_edges;
+    MeshTopology mesh(V, F);
+    mesh.inner_edges(in_edges);
+    mesh.boundary_edges(bnd_edges);
+    cout << mesh.edges.cols() << " " << in_edges.cols() << " " << bnd_edges.cols() << endl;
     return 0;
 }
